062d36437ab4fe8a5a919c0fb5bcf14342a19cef
megha8191 <meghachhabra819@gmail.com>
2024-05-07 02:02:14 +0530

    dashboard access after login only

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -25,7 +25,6 @@ const Signup = ({setIslogin}) => {
                [name]: value
             }
         ))
-        console.log(signdata)
     }
     const signupSumit=(e)=>{
         e.preventDefault();
